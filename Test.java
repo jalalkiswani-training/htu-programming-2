6ca07e68d60b4d3844c05ee2b7858ed13111deaf
Jalal Kiswani <kiswanij@gmail.com>
2020-03-18 09:19:57 +0200

    Update to Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,14 @@
+/*
+The is the first Java class
+*/
 public class Test{
+	
+	// This is the main method
 	public static void main(String args[]){
 		int num1=10;
 		int num2=20;
 		int sum=num1+num2;
 		System.out.println("The sum is :"+sum);
 	}
+	
 }
