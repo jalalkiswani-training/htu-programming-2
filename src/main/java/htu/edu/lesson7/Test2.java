f371c76f74957ebc42d5c8781adc4361b68fb3d8
Jalal Kiswani <kiswanij@gmail.com>
2020-04-20 09:52:41 +0300

    Functions/Methods and Procedural Programming

---
+++ src/main/java/htu/edu/lesson7/Test2.java
@@ -0,0 +1,10 @@
+package htu.edu.lesson7;
+
+public class Test2 {
+	public static void main(String[] args) {
+		int n1=Test.readNumber();
+		int n2=Test.readNumber();
+		
+		System.out.println(n1*n2);
+	}
+}
